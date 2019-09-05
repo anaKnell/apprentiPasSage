9704a2c5e4217f25a0e9f8af1ffa512ed7fc1c20
Anaelle <anaelle.alenda@gmail.com>
2019-09-05 08:59:01 +0200

    Home_BD_style_prestation

---
+++ resources/views/pages/accueil/accueil-PrestationCard.blade.php
@@ -0,0 +1,9 @@
+<div class="col-md themed-grid-col">
+	<div class="card m-1 d-flex align-items-center" id="cardPrestation" style="width: 17rem; height: 18rem;">
+		<img class="card-img-top m-3" src="img/{{$cat->categories_img}}" alt=" image de la catégorie de prestation :{{$nameCat->categories_name}}" style="width: 7rem; height: 7rem;">
+		<div class="card-body cardPrestation">
+			<h1 class="titlePrestation card-text text-center m-2">{{$nameCat->categories_name}}</h1>
+			<a href="" class="btn btn-warning d-flex align-items-center">LINK{{--{{$cat->link}}--}}</a>
+		</div>
+	</div>
+</div>
